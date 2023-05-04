8e60367ea0801f3df2313f4f09653e2db7f8ac52
Hector Salguero <78835095+1andres5s@users.noreply.github.com>
2023-05-04 12:39:23 -0500

    taller ajax 1

--- ejercicio/ajaxDOM.js
+++ ejercicio/ajaxDOM.js
@@ -3,25 +3,7 @@ const ajax = new XMLHttpRequest(), //Variable ajax que instancia del objeto XMLH
     $lista = document.getElementById("lista"), //variable que hace referencia al nodo ol del HTML id="lista"
     $fragmento = document.createDocumentFragment(); //Fragmento en donde se guardaran las listas
 
-//Asignación de los eventos que se vayan a manipular en la petición
-ajax.addEventListener("readystatechange", (e) => { 
-    
-    if (ajax.readyState !== 4) return; 
-
-    if (ajax.status >= 200 && ajax.status < 300) { //Si la respuesta es satisfactoria
-        let json = JSON.parse(ajax.responseText); //Convierte la respuesta Ajax en un objeto
-
-        for (const key in json) { //recorre el objeto
-            console.log(`Llave: ${key}, Valor: ${json[key]}`)
-            const $li = document.createElement("li"); //Crea el elemento li 
-            $li.innerHTML = `${key}: ${json[key]}`; //Le asigna los valores del objeto al elemento li
-            $fragmento.appendChild($li); //Agrega el elemento li al framento para luego insertarlo en el HTML
-        }
-        $lista.appendChild($fragmento); //Cuando termina de recorrer el objeto agrega los elementos li guardados en el fragmento al elemento lista
-    } else { //Si la respuesta no es satisfactoria manda un mensaje de error
-        let message = ajax.statusText || "Ocurrió un error";
-        $lista.innerHTML = `Error ${ajax.status}: ${message}`;
-    }
+function estatus()  {
     console.log("---------------respuesta------------")
     console.log(ajax.responseText)
     console.log(`La URL de la peticion es: ${ajax.responseURL}`)
@@ -48,9 +30,37 @@ ajax.addEventListener("readystatechange", (e) => {
     }else if(respuesta==500){
         console.log("error interno del servidor.")
     }
+
+
+}
+//Asignación de los eventos que se vayan a manipular en la petición
+ajax.addEventListener("readystatechange", (e) => { 
+    
+    if (ajax.readyState !== 4) return; 
+
+    if (ajax.status >= 200 && ajax.status < 300) { //Si la respuesta es satisfactoria
+        let json = JSON.parse(ajax.responseText); //Convierte la respuesta Ajax en un objeto
+
+        for (const key in json) { //recorre el objeto
+            console.log(`Llave: ${key}, Valor: ${json[key]}`)
+            const $li = document.createElement("li"); //Crea el elemento li 
+            $li.innerHTML = `${key}: ${json[key]}`; //Le asigna los valores del objeto al elemento li
+            $fragmento.appendChild($li); //Agrega el elemento li al framento para luego insertarlo en el HTML
+        }
+        $lista.appendChild($fragmento); //Cuando termina de recorrer el objeto agrega los elementos li guardados en el fragmento al elemento lista
+    } else { //Si la respuesta no es satisfactoria manda un mensaje de error
+        let message = ajax.statusText || "Ocurrió un error";
+        $lista.innerHTML = `Error ${ajax.status}: ${message}`;
+    }
+
+    estatus()
+
+
+    console.log("----------segunda parte-------------")
+    
 });
 
 /*Abre la petición con el método GET.*/
-ajax.open("GET", "https://jsonplaceholder.typicode.com/posts/26");
+ajax.open("GET", "https://jsonplaceholder.typicode.com/comments/63");
 /*envia la peticioN*/
 ajax.send();
